bfee1b50d48f5e8aa083bf7b2fdee5371549141b
Gindila Catalin <catalin.gandila@gmail.com>
2026-04-24 19:23:04 +0300

    first commot

---
+++ Vending Machine/VendingMachine/Authentication/AuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿namespace iQuest.VendingMachine.Authentication
+{
+    internal class AuthenticationService
+    {
+        public bool IsUserAuthenticated { get; private set; }
+
+        public void Login(string password)
+        {
+            if (password == "supercalifragilisticexpialidocious")
+                IsUserAuthenticated = true;
+            else
+                throw new InvalidPasswordException();
+        }
+
+        public void Logout()
+        {
+            IsUserAuthenticated = false;
+        }
+    }
+}
